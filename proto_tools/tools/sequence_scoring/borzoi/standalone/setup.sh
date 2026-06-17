d5b536b0f78b0867bb0400a084fab8004084a77b
Ben Viggiano <viggiano.benjamin@gmail.com>
2026-06-17 14:58:16 -0700

    Update

--- proto_tools/tools/sequence_scoring/borzoi/standalone/setup.sh
+++ proto_tools/tools/sequence_scoring/borzoi/standalone/setup.sh
@@ -8,7 +8,7 @@ echo "Installing uv package manager..."
 pip install uv
 
 echo "Installing PyTorch and borzoi-pytorch..."
-uv pip install torch==2.7.1 borzoi-pytorch --extra-index-url "${RECOMMENDED_TORCH_INDEX}"
+uv pip install torch==2.7.1 borzoi-pytorch==0.5.1 --extra-index-url "${RECOMMENDED_TORCH_INDEX}"
 
 ARCH=$(uname -m)
 if [ "$ARCH" = "aarch64" ]; then
